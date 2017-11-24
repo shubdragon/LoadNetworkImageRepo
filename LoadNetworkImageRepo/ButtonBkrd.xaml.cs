2c0772b110975efd7094c41c77b29aa194e40fdb
shubdragon <shubham.sahu@hotmail.com>
2017-11-24 13:16:31 +0530

    Initial commit

---
+++ LoadNetworkImageRepo/ButtonBkrd.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LoadNetworkImageRepo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ButtonBkrd : Page
+    {
+        public ButtonBkrd()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private async void UserButton2_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(@"\\SharedPC\Users\SharedContent\media"); //Your Location. you need to share respective location else it will through exception.
+            StorageFile file = await folder.GetFileAsync("placeholder-sdk.png"); //Your Image
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                await bitmapImage.SetSourceAsync(stream);                
+                var brush = new ImageBrush();
+                if (bitmapImage != null)
+                {
+                    brush.ImageSource = bitmapImage;
+                    UserButton2.Content = "";
+                    UserButton2.Background = brush;
+                }
+            }
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
+        }
+    }
+}
---
+++ LoadNetworkImageRepo/ImageBkgrd.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LoadNetworkImageRepo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ImageBkgrd : Page
+    {
+        public ImageBkgrd()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void UserButton2_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(@"\\YouPC\Users\SharedContent\media"); // Your Location. you need to share respective location else it will through exception.
+            StorageFile file = await folder.GetFileAsync("placeholder-sdk.png"); // Your Image.
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.Read))
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                await bitmapImage.SetSourceAsync(stream);                                
+                if (bitmapImage != null)
+                {
+                    UserImage1.Source = bitmapImage;
+                }
+            }
+        }
+
+        private void HomeButton2_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
+        }
+    }
+}
